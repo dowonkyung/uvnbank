85ca5a1142683ebed9e423734260da2dd1ceb80a
dowonkyung <doxxxkyung@gmail.com>
2025-11-11 20:15:09 +0900

    Update firebase.js

--- web/src/firebase.js
+++ web/src/firebase.js
@@ -5,10 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getFunctions } from 'firebase/functions';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCYww5IU62dYEEIQps5qPqK6BY4z1eSA_U",
+  authDomain: "uvnbank.firebaseapp.com",
+  projectId: "uvnbank",
+  storageBucket: "uvnbank.firebasestorage.app",
+  messagingSenderId: "1085684290658",
+  appId: "1:1085684290658:web:1434cd13716e3e3a77ed3b",
+  measurementId: "G-HTDCR7XQ1X"
 };
 
 export const app = initializeApp(firebaseConfig);
