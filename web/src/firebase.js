72d0b2d9ad51248eb4edf1b3e4b7a2af6fff6fb5
dowonkyung <doxxxkyung@gmail.com>
2025-11-11 20:03:48 +0900

    Create firebase.js

---
+++ web/src/firebase.js
@@ -0,0 +1,17 @@
+// Firebase 콘솔에서 설정 복사해서 채워넣기
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getFunctions } from 'firebase/functions';
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT",
+  appId: "YOUR_APP_ID"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const functions = getFunctions(app);
